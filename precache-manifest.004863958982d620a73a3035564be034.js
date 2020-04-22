6bf0355790f29a5ce3aff6c2ba7910ad1d84c1f0
Muhammad Sameer Bin Raees <44906406+sameerbinraees@users.noreply.github.com>
2020-04-23 04:48:04 +0500

    Updates

---
+++ precache-manifest.004863958982d620a73a3035564be034.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edfb16a8e72f0dc780c88c4d2d456d5b",
+    "url": "/LCO-Basketball/index.html"
+  },
+  {
+    "revision": "614be2f224af20cfa77e",
+    "url": "/LCO-Basketball/static/css/2.d1551ae5.chunk.css"
+  },
+  {
+    "revision": "f9d72687a517eedbb6a7",
+    "url": "/LCO-Basketball/static/css/main.c09738ed.chunk.css"
+  },
+  {
+    "revision": "614be2f224af20cfa77e",
+    "url": "/LCO-Basketball/static/js/2.4613c4ec.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/LCO-Basketball/static/js/2.4613c4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d72687a517eedbb6a7",
+    "url": "/LCO-Basketball/static/js/main.d4ae8222.chunk.js"
+  },
+  {
+    "revision": "3c0eae9a2d33280c3129",
+    "url": "/LCO-Basketball/static/js/runtime-main.12323839.js"
+  },
+  {
+    "revision": "4a32f4b41ca0d8fe0f72cd5d604ec758",
+    "url": "/LCO-Basketball/static/media/Big Tournament Coming Soon.4a32f4b4.png"
+  },
+  {
+    "revision": "5dd6f64a22b7874b01ada0c2ed58be2e",
+    "url": "/LCO-Basketball/static/media/artimg-2.5dd6f64a.png"
+  },
+  {
+    "revision": "b045915f404262e624592bc6c13a7230",
+    "url": "/LCO-Basketball/static/media/basketball-on-pitch.b045915f.jpg"
+  },
+  {
+    "revision": "893ef9faa1d9e60c038cdbe39c723416",
+    "url": "/LCO-Basketball/static/media/hoop.893ef9fa.jpg"
+  },
+  {
+    "revision": "ff463063f08d018b739af747f966ec30",
+    "url": "/LCO-Basketball/static/media/img1(cropped).ff463063.png"
+  },
+  {
+    "revision": "2839ec9f33751f062c805a14ced0ab31",
+    "url": "/LCO-Basketball/static/media/img2(cropped).2839ec9f.png"
+  },
+  {
+    "revision": "bd859020306b69dcdc5b6d16aea0e9e4",
+    "url": "/LCO-Basketball/static/media/img3(cropped).bd859020.png"
+  },
+  {
+    "revision": "fba0f253c71f19aaec7ac7d35e7e4cb9",
+    "url": "/LCO-Basketball/static/media/img4(cropped).fba0f253.png"
+  },
+  {
+    "revision": "a7220f8ca3aedeb9cd6c5592144b577e",
+    "url": "/LCO-Basketball/static/media/sports-logo-transparent.a7220f8c.png"
+  },
+  {
+    "revision": "dd7a7238178d5bef5f2a5fdef8d60483",
+    "url": "/LCO-Basketball/static/media/tree.dd7a7238.jpg"
+  }
+]);
